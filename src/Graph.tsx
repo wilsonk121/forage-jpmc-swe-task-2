dd2cbb836d1307de9292b5a3ebb8b48ed694a6fd
wilsonk121 <wilsonlj@connect.hku.hk>
2023-08-29 11:39:42 +0800

    debugging1

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -34,7 +34,6 @@ class Graph extends Component<IProps, {}> {
     // Get element to attach the table from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
@@ -49,11 +48,13 @@ class Graph extends Component<IProps, {}> {
     if (this.table) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
-      elem.setAttribute('view','y_line');
-      elem.setAttribute('column_pivots','["stock"]');
-      elem.setAttribute('row_pivots','["timestamp"]');
-      elem.setAttribute('columns','["top_ask_price"]');
-      elem.setAttribute('aggregates',`{"stock":"distinct count","top_ask_price":"avg","top_bid_price":"avg","timestamp":"distinct count"}`);
+            elem.setAttribute("view", "y_line");
+            elem.setAttribute("column-pivots", '["stock"]');
+            elem.setAttribute("row_pivots", '["timestamp"]');
+            elem.setAttribute("columns", '["top_ask_price"]');
+            elem.setAttribute("aggregates",
+                '{"stock":"distinct_count", "top_ask_price":"avg", "top_bid_price":"avg", "timestamp":"distinct_count"}'
+            );
       // Add more Perspective configurations here.
 
     }
