877faa655742a7ff6f7f242eff5e37489f021ec2
wilsonk121 <wilsonlj@connect.hku.hk>
2023-08-29 11:15:45 +0800

    changed to dynamic graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -32,7 +32,15 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    elem.setAttribute('view','y_line');
+    elem.setAttribute('column_pivots','["stock"]');
+    elem.setAttribute('row_pivots','["timestamp'"]');
+    elem.setAttribute('columns','["top_ask_price"]');
+    elem.setAttribute('aggregates','{"stock":"distinct count",
+    "top_ask_price":"avg",
+    "top_bid_price":"avg",
+    "timestamp":"distinct count"}');
 
     const schema = {
       stock: 'string',
